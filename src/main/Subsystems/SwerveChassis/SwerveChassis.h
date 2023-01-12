28104ced4499a720fb299c1a74177311b5b8a13f
Luis Leyva <70904067+Luis-Leyva@users.noreply.github.com>
2023-01-12 15:37:27 -0600

    Fixed Includes Errors

--- src/main/Subsystems/SwerveChassis/SwerveChassis.h
+++ src/main/Subsystems/SwerveChassis/SwerveChassis.h
@@ -4,21 +4,21 @@
 
 #pragma once
 
-#include <frc2/command/SubsystemBase.h>
 #include <Subsystems/SwerveModule/SwerveModule.h>
-#include <frc/Joystick.h>
-#include <frc2/command/SubsystemBase.h>
-#include <frc/smartdashboard/SmartDashboard.h>
-//#include <frc/filter/SlewRateLimiter.h>
-#include <frc/kinematics/ChassisSpeeds.h>
-#include <frc/kinematics/SwerveDriveKinematics.h>
-#include <frc/kinematics/SwerveDriveOdometry.h>
-#include <frc/estimator/SwerveDrivePoseEstimator.h>
-#include <frc/smartdashboard/Field2d.h>
-#include <frc/geometry/Translation2d.h>
 
+#include <frc/Joystick.h>
+#include <frc/geometry/Translation2d.h>
+#include <frc/geometry/Pose2d.h>
+#include <frc/kinematics/SwerveDriveKinematics.h>
+#include <frc/kinematics/ChassisSpeeds.h>
+#include <frc/kinematics/SwerveDriveOdometry.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <frc/smartdashboard/Field2d.h>
+#include <frc/estimator/SwerveDrivePoseEstimator.h>
+#include <frc2/command/SubsystemBase.h>
 #include <numbers>
 #include <AHRS.h>
+//#include <frc/filter/SlewRateLimiter.h>
 
 class SwerveChassis : public frc2::SubsystemBase {
  public:
@@ -30,7 +30,7 @@ class SwerveChassis : public frc2::SubsystemBase {
   void Periodic() override;
 
  private:
- SwerveModule backRightModule{ 1, 2, 9, -143.70507812500001 };
+  SwerveModule backRightModule{ 1, 2, 9, -143.70507812500001 };
   SwerveModule backLeftModule{ 3, 4, 10, -70 };
   SwerveModule frontLeftModule{ 5, 6, 11, -147.5 };
   SwerveModule frontRightModule{ 7, 8, 12, -160 };
@@ -48,11 +48,11 @@ class SwerveChassis : public frc2::SubsystemBase {
       frc::Translation2d(-10.36_in, -10.36_in), // back right
       frc::Translation2d(-10.36_in, 10.36_in)   // back left
   };
+
   AHRS navx{ frc::SPI::Port::kMXP };
   frc::SwerveDriveKinematics<4> kinematics{ modulePos };
 
-  frc::SwerveDrivePoseEstimator<4> odometry{ frc::Rotation2d(0_deg), {0_m,0_m,{0_deg}}, kinematics, {0.6,0.6,0.6}, {0.5},{0.5,0.5,0.5} };
+  frc::SwerveDrivePoseEstimator<4> odometry{ kinematics, frc::Rotation2d{}, modulePos, frc::Pose2d{}};
 
-  frc::Field2d field2d;
-
+  frc::Field2d field2d;
 };
