e3b0007b6ca70baa08373d7281d53d1219867b0c
David Soni <sonidavid46@gmail.com>
2023-01-24 16:27:57 -0600

    Intake created and objects defined

---
+++ src/main/Subsystems/Intake.h
@@ -0,0 +1,28 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <frc2/command/SubsystemBase.h>
+#include <ctre/Phoenix.h>
+#include <frc/Solenoid.h>
+
+class Intake : public frc2::SubsystemBase {
+ public:
+  Intake();
+ 
+ 
+virtual void frc::Solenoid::Set	(	bool 	off	)	
+/*void frc::Solenoid::Toggle	(		)	
+esto con un "if"? */
+  
+  void Periodic() override;
+
+ private:
+ 
+ WPI_TalonFX leftIntakeMotor {13};
+ WPI_TalonFX rightIntakeMotor  {14};  
+ frc::Solenoid::Solenoid intakeSolenoid {frc::PneumaticsModuleType::CTREPCM, 1, 2};
+ 
+};
