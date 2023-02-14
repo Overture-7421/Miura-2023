16f583385e437bf1599b003600480fb228a0c767
DiegoArmandoFondon <a01199394@tec.mx>
2023-02-13 19:20:08 -0600

    Arm Modification (formulas)

---
+++ src/main/Subsystems/DoubleArm/DoubleArm.cpp
@@ -0,0 +1,31 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "DoubleArm.h"
+#include <math.h>
+#include <cmath>
+
+DoubleArm::DoubleArm() = default;
+
+// This method will be called once per scheduler run
+void DoubleArm::Periodic() {}
+void SetArmPosition(double Angle1, double Angle2, double TargetX, double TargetY) {
+    double Length1 = 33;  //inches
+    double Length2 = 33;  //inches
+
+    double x = cos(Angle1) * Lenght1 + cos(Angle2) * Lenght2;
+    double y = sin(Angle1) * Lenght1 + sin(Angle2) * Lenght2;
+
+    double TargetAngle2 = (acos((TargetX ^ 2 + TargetY ^ 2 - Length1 ^ 2 - Length2 ^ 2) / (2 * Length1 * Length2))) * -1;
+    double TargetAngle1 = atan2(TargetY / TargetX) + atan2((Length2 * sin(TargetAngle2)) / (Length1 + Length2 * cos(TargetAngle2)));
+
+    double FinalMovementAngle2 = TargetAngle2 - Angle2;
+    double FinalMovementAngle1 = TargetAngle1 - Angle1;
+}
+
+
+//  acos(x) = cos^-1(x)
+//  atan2(x) = tan^-1(x)
+//  sin(x) = sin x
+//  cos(x) = cos x
