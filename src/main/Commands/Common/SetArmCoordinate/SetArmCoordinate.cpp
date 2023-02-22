b3c6bff0cf6cb6483596e5a27aad2848298f3d0a
Rigo-Soto <91226949+Rigo-Soto@users.noreply.github.com>
2023-02-22 04:06:27 -0600

    SetArmCommand Created

---
+++ src/main/Commands/Common/SetArmCoordinate/SetArmCoordinate.cpp
@@ -0,0 +1,28 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "SetArmCoordinate.h"
+
+SetArmCoordinate::SetArmCoordinate(DoubleArm* doubleArm, frc::Translation2d target) {
+    this->doubleArm = doubleArm;
+    this->target = target;
+
+    AddRequirements(doubleArm);
+}
+
+// Called when the command is initially scheduled.
+void SetArmCoordinate::Initialize() {
+    doubleArm->SetTargetCoord(target);
+}
+
+// Called repeatedly when this Command is scheduled to run
+void SetArmCoordinate::Execute() {}
+
+// Called once the command ends or is interrupted.
+void SetArmCoordinate::End(bool interrupted) {}
+
+// Returns true when the command should end.
+bool SetArmCoordinate::IsFinished() {
+    return target.Distance(doubleArm->GetEndpointCoord()) < .1_m;
+}
---
+++ src/main/Commands/Common/SetArmCoordinate/SetArmCoordinate.h
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <frc2/command/CommandBase.h>
+#include <frc2/command/CommandHelper.h>
+#include "Subsystems/DoubleArm/DoubleArm.h"
+#include <frc/geometry/Translation2d.h>
+
+/**
+ * An example command.
+ *
+ * <p>Note that this extends CommandHelper, rather extending CommandBase
+ * directly; this is crucially important, or else the decorator functions in
+ * Command will *not* work!
+ */
+class SetArmCoordinate
+    : public frc2::CommandHelper<frc2::CommandBase, SetArmCoordinate> {
+public:
+    SetArmCoordinate(DoubleArm* doubleArm, frc::Translation2d target);
+
+    void Initialize() override;
+
+    void Execute() override;
+
+    void End(bool interrupted) override;
+
+    bool IsFinished() override;
+
+private:
+    DoubleArm* doubleArm;
+    frc::Translation2d target;
+
+};
