bc8ada2f70936403a92d81fa960aaba4218bb798
Luis Leyva <70904067+Luis-Leyva@users.noreply.github.com>
2023-02-23 23:18:04 -0600

    Updated CAN updtes

--- src/main/Subsystems/SwerveModule/SwerveModule.cpp
+++ src/main/Subsystems/SwerveModule/SwerveModule.cpp
@@ -14,6 +14,12 @@ SwerveModule::SwerveModule(int rotatorID, int wheelID, int canCoderID, double of
     canCoder.ConfigSensorDirection(false);
     rotator.SetInverted(true);
 
+    wheel.SetStatusFramePeriod(ctre::phoenix::motorcontrol::StatusFrameEnhanced::Status_1_General, 20);
+    wheel.SetStatusFramePeriod(ctre::phoenix::motorcontrol::StatusFrameEnhanced::Status_2_Feedback0, 255);
+
+    rotator.SetStatusFramePeriod(ctre::phoenix::motorcontrol::StatusFrameEnhanced::Status_1_General, 20);
+    rotator.SetStatusFramePeriod(ctre::phoenix::motorcontrol::StatusFrameEnhanced::Status_2_Feedback0, 255);
+
     wheel.SetNeutralMode(NeutralMode::Brake);
     rotator.SetNeutralMode(NeutralMode::Coast);
     wheel.SetSelectedSensorPosition(0);
