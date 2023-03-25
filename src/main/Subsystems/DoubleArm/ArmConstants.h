5af4468868cdf6d28f63624aa30bc3ab09340fd8
Luis Leyva <70904067+Luis-Leyva@users.noreply.github.com>
2023-03-25 01:16:51 -0600

    Mid Arm Change in Y

--- src/main/Subsystems/DoubleArm/ArmConstants.h
+++ src/main/Subsystems/DoubleArm/ArmConstants.h
@@ -13,7 +13,7 @@ namespace ArmConstants {
     namespace Positions {
         const frc::Translation2d closed{ 0.11_m, 0.02_m };
         const frc::Translation2d upper{ 1_m, 0.80_m };
-        const frc::Translation2d middle{ 0.55_m, 0.25_m };
+        const frc::Translation2d middle{ 0.55_m, 0.22_m };
         const frc::Translation2d ground{ 1.02_m, -.54_m };
         const frc::Translation2d portal{ 0.87_m, 0.38_m };
 
